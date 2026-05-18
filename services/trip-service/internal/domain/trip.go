b43537ad7bb40e02cb27454fba91e05ebae7974c
unknown <mutheedavid12@gmail.com>
2026-05-18 20:01:54 +0300

    feat(trip-service): fetch OSRM route previews

--- services/trip-service/internal/domain/trip.go
+++ services/trip-service/internal/domain/trip.go
@@ -2,14 +2,15 @@ package domain
 
 import (
 	"context"
+	"ride-sharing/shared/types"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type TripModel struct {
-	ID primitive.ObjectID
-	UserID string
-	Status string
+	ID       primitive.ObjectID
+	UserID   string
+	Status   string
 	RideFare *RideFareModel
 }
 
@@ -17,6 +18,7 @@ type TripRepository interface {
 	CreateTrip(ctx context.Context, trip *TripModel) (*TripModel, error)
 }
 
-type TripService interface{
-	CreateTrip(ctx context.Context, fare RideFareModel) (*TripModel, error)
+type TripService interface {
+	CreateTrip(ctx context.Context, fare *RideFareModel) (*TripModel, error)
+	GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*types.OsrmApiResponse, error)
 }
