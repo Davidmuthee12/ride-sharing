3a01b67f7da7ccdafcbb9a93a051c3a7a56089d4
unknown <mutheedavid12@gmail.com>
2026-05-22 12:59:48 +0300

    chore(web-src): add type declarations for web/src/assets.d.ts

---
+++ web/src/assets.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg" {
+    const content: { src: string };
+    export default content;
+}
+
+declare module "*.png" {
+    const content: { src: string };
+    export default content;
+}
