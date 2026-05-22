6310b7b9483fd80efa95d7bdcaf6f5992be21da5
unknown <mutheedavid12@gmail.com>
2026-05-22 12:59:45 +0300

    chore(services-api-gateway): update api gateway file services/api-gateway/types.go

--- services/api-gateway/types.go
+++ services/api-gateway/types.go
@@ -6,8 +6,8 @@ import (
 )
 
 type previewTripRequest struct {
-	UserID string `json:"userID"`
-	Pickup types.Coordinate `json:"pickup"`
+	UserID      string           `json:"userID"`
+	Pickup      types.Coordinate `json:"pickup"`
 	Destination types.Coordinate `json:"destination"`
 }
 
@@ -15,11 +15,11 @@ func (p *previewTripRequest) toProto() *pb.PreviewTripRequest {
 	return &pb.PreviewTripRequest{
 		UserID: p.UserID,
 		StartLocation: &pb.Coordinate{
-			Latitude: p.Pickup.Latitude,
+			Latitude:  p.Pickup.Latitude,
 			Longitude: p.Pickup.Longitude,
 		},
 		EndLocation: &pb.Coordinate{
-			Latitude: p.Destination.Latitude,
+			Latitude:  p.Destination.Latitude,
 			Longitude: p.Destination.Longitude,
 		},
 	}
