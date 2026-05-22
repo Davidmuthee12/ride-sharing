81859f5d78569526a34bb717f535ae9746d69551
unknown <mutheedavid12@gmail.com>
2026-05-22 16:06:12 +0300

    feat(trip-repo): implement in-memory fare lookup by user

--- services/trip-service/internal/infrastructure/repository/inmem.go
+++ services/trip-service/internal/infrastructure/repository/inmem.go
@@ -2,28 +2,37 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"ride-sharing/services/trip-service/internal/domain"
 )
 
 type inmemRepository struct {
-	trips    map[string]*domain.TripModel
+	trips     map[string]*domain.TripModel
 	rideFares map[string]*domain.RideFareModel
 }
 
 func NewInmemRepository() *inmemRepository {
-	return &inmemRepository {
-		trips: make(map[string]*domain.TripModel),
+	return &inmemRepository{
+		trips:     make(map[string]*domain.TripModel),
 		rideFares: make(map[string]*domain.RideFareModel),
 	}
 }
 
- func (r *inmemRepository) CreateTrip(ctx context.Context, trip *domain.TripModel) (*domain.TripModel, error) {
-	 r.trips[trip.ID.Hex()] = trip
-	 return trip, nil
- }
+func (r *inmemRepository) GetRideFareByID(ctx context.Context, id string) (*domain.RideFareModel, error) {
+	fare, exist := r.rideFares[id]
+	if !exist {
+		return nil, fmt.Errorf("fare does not exist with ID: %s", id)
+	}
 
- func (r *inmemRepository) SaveRideFare(ctx context.Context, f *domain.RideFareModel) error {
+	return fare, nil
+}
+
+func (r *inmemRepository) CreateTrip(ctx context.Context, trip *domain.TripModel) (*domain.TripModel, error) {
+	r.trips[trip.ID.Hex()] = trip
+	return trip, nil
+}
+
+func (r *inmemRepository) SaveRideFare(ctx context.Context, f *domain.RideFareModel) error {
 	r.rideFares[f.ID.Hex()] = f
-
 	return nil
- }
+}
