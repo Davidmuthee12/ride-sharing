69ba62b75530c0a08785f520cd07efd24dc5038a
unknown <mutheedavid12@gmail.com>
2026-05-28 13:45:05 +0300

    chore(repo): update infra/development/docker/driver-build.bat

--- infra/development/docker/driver-build.bat
+++ infra/development/docker/driver-build.bat
@@ -3,4 +3,4 @@ set CGO_ENABLED=0
 set GOOS=linux
 set GOARCH=amd64
 if not exist build mkdir build
-go build -buildvcs=false -o build/driver-service ./services/driver-service/main.go
+go build -buildvcs=false -o build/driver-service ./services/driver-service/
