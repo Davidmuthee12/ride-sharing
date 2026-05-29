ab0d70f99554dd06b135318274e1dfa6c21c8fa6
unknown <mutheedavid12@gmail.com>
2026-05-29 13:16:48 +0300

    refactor(trip-service): rename ride fare proto helper

--- services/trip-service/internal/domain/ride_fare.go
+++ services/trip-service/internal/domain/ride_fare.go
@@ -6,29 +6,28 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
 type RideFareModel struct {
 	ID                primitive.ObjectID
 	UserID            string
-	PackageSlug       string //ex: van, Luxury, sedan
+	PackageSlug       string // ex: van, luxury, sedan
 	TotalPriceInCents float64
-	Route *types.OsrmApiResponse
+	Route             *types.OsrmApiResponse
 }
 
 func (r *RideFareModel) ToProto() *pb.RideFare {
 	return &pb.RideFare{
-		Id: r.ID.Hex(),
-		UserID: r.UserID,
-		PackageSlug: r.PackageSlug,
+		Id:                r.ID.Hex(),
+		UserID:            r.UserID,
+		PackageSlug:       r.PackageSlug,
 		TotalPriceInCents: r.TotalPriceInCents,
 	}
 }
 
-func ToRideFareProto(fares []*RideFareModel) []*pb.RideFare {
+func ToRideFaresProto(fares []*RideFareModel) []*pb.RideFare {
 	var protoFares []*pb.RideFare
-
 	for _, f := range fares {
 		protoFares = append(protoFares, f.ToProto())
 	}
-
 	return protoFares
 }
